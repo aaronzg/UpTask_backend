212a5a609ee31b7b6a96368baa51fb81f8fe6e56
zgaaron <aaronchavez966@gmail.com>
2026-01-04 07:10:13 -0600

    Add validation to create-account endpoint

--- src/Routes/authRoutes.ts
+++ src/Routes/authRoutes.ts
@@ -1,7 +1,23 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.get('/', (req, res) => res.send('Desde /api/auth'))
+router.get('/create-account', 
+  body('name')
+  .notEmpty().withMessage('El nombre no puede estar vacio'),
+  body('password')
+  .isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres'),
+  body('password-confirmation')
+  .custom((value, { req }) => {
+    if(value !== req.body.password) throw new Error('Las contraseñas no coinciden')
+    return true
+  }),
+  body('email')
+  .isEmail().withMessage('Email no valido'),
+  handleInputErrors,
+  AuthController.createAcount)
 
 export default router
---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express"
+
+export class AuthController {
+  static createAcount = (req: Request, res: Response) => {
+    
+  }
+}
+
